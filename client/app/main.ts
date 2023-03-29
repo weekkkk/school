5fa256a698fe16dac432c1ef03deb58c78c0583f
afxsta <devefosta@gmail.com>
2023-03-29 22:13:34 +0300

    monsterrat font

--- client/app/main.ts
+++ client/app/main.ts
@@ -3,6 +3,8 @@ import App from './app.vue'
 import RegisterComponents from "./plugins/register-components"
 import StyleVars from "./styles/StyleVars"
 
+import "./assets/fonts/monsterrat/style.css"
+
 StyleVars.setVars(new StyleVars());
 const app = createApp(App)
 app.use(RegisterComponents);
