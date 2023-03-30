24ddb84875b4e381915c982ad3fcb0ee898f8742
afxsta <devefosta@gmail.com>
2023-03-30 23:17:07 +0300

    results page

--- client/pages/common/results.vue
+++ client/pages/common/results.vue
@@ -1,18 +1,37 @@
 <template>
     <div class="results">
-        <ui-dropdown :items="items" :selectedId="selectedId" @select="selectedId = $event" />
+        <ui-label name="Школа">
+            <ui-dropdown :items="items" :selectedId="selectedId" @select="selectedId = $event" />
+        </ui-label>
+        <ui-label name="Предмет" borderBottom>
+            <ui-dropdown :items="items" :selectedId="selectedId" @select="selectedId = $event" />
+        </ui-label>
+        <div class="show-results-button-wrapper">
+            <ui-button>
+                Показать результаты
+            </ui-button>
+        </div>
+        <ui-table :data="data" />
     </div>
 </template>
 <script lang="ts" setup>
 import { ref } from 'vue';
 
-const items = ref([
-    { Id: 0, Name: "qwe" },
-    { Id: 1, Name: "asd" },
-    { Id: 2, Name: "zxc" },
-    { Id: 3, Name: "cxz" },
-    { Id: 4, Name: "dsa" },
-])
+const items = ref([])
 const selectedId = ref(0)
+const data =
+    [
+        ["ФИО", "Вариант 1", "Вариант 2", "Вариант 3", "Вариант 4"],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""]
+    ]
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.results {
+    .show-results-button-wrapper {
+        display: flex;
+        margin: 16px 0 24px;
+        justify-content: flex-end;
+    }
+}
+</style>
