0c6b37a7c704bb0a66ba939e0c6d696f9230226a
afxsta <devefosta@gmail.com>
2023-04-02 17:48:05 +0300

    icons import

--- client/shared/ui/ui-table.vue
+++ client/shared/ui/ui-table.vue
@@ -5,12 +5,20 @@
                 {{ cell }}
             </div>
             <template v-if="showEditButtons">
-                <div class="ui-table-cell" :style="cellStyle">{{ index ? "Удалить" : "" }}</div>
-                <div class="ui-table-cell" :style="cellStyle">{{ index ? "Изменить" : "" }}</div>
+                <div class="ui-table-cell additional">
+                    <img v-if="index" src="../../app/assets/images/delete.png" alt="delete">
+                </div>
+                <div class="ui-table-cell additional">
+                    <img v-if="index" src="../../app/assets/images/edit.png" alt="edit">
+                </div>
             </template>
             <template v-if="showPreviewButtons">
-                <div class="ui-table-cell" :style="cellStyle">{{ index ? "Скачать" : "" }}</div>
-                <div class="ui-table-cell" :style="cellStyle">{{ index ? "Посмотреть ответы" : "" }}</div>
+                <div class="ui-table-cell additional">
+                    <img v-if="index" src="../../app/assets/images/download.png" alt="download">
+                </div>
+                <div class="ui-table-cell additional">
+                    <img v-if="index" src="../../app/assets/images/preview.png" alt="preview">
+                </div>
             </template>
         </div>
     </div>
@@ -41,17 +49,23 @@ const rows = computed(() => {
     let rowLength = props.data[0].length;
     return props.data.map(d => d.filter((x, i) => i < rowLength));
 })
-/**Количество ячеек */
-const cellsCount = computed(() => {
+/**Количество доп. ячеек */
+const additionalCells = computed(() => {
     let additionalCells = 0
     if (props.showEditButtons) additionalCells += 2;
     if (props.showPreviewButtons) additionalCells += 2;
-    return rows.value[0].length + additionalCells;
+    return additionalCells;
+})
+/**Количество ячеек */
+const cellsCount = computed(() => {
+    return rows.value[0].length + additionalCells.value;
 })
 /**Стили для ячейки */
-const cellStyle = computed(() => ({
-    "min-width": 100 / cellsCount.value + "%"
-}))
+const cellStyle = computed(() => {
+    let mainCells = cellsCount.value - additionalCells.value;
+    let additionalCellsPx = additionalCells.value * 50 / mainCells;
+    return { "min-width": `calc(${100 / mainCells}% - ${additionalCellsPx}px)` }
+})
 
 /**Получение класса для ряда */
 const getRowClass = (index: number) => {
@@ -95,6 +109,16 @@ const getRowClass = (index: number) => {
             width: 100%;
             background-color: var(--color-default);
             overflow: hidden;
+
+            &.additional {
+                max-width: 50px;
+                cursor: pointer;
+
+                img {
+                    max-height: 30px;
+                    width: auto;
+                }
+            }
         }
     }
 }
