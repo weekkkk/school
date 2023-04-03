c1fd35f6b6aa819bff59efc422f9fa99bf5ff635
weekkkk <nikitanedelko9@gmail.com>
2023-04-03 04:15:48 +0300

    удалил лишнюю папку

--- client/app/router/router-names.ts
+++
@@ -1,8 +0,0 @@
-export const MAIN = "main";
-export const RESULTS = "results";
-export const CREATE_TEST = "create-test";
-export const CREATE_USER = "create-user";
-export const CREATE_SCHOOL = "create-school";
-export const CREATE_CLASS = "create-class";
-export const CREATE_MATERIAL = "create-material";
-export const VARIANTS = "variants";
